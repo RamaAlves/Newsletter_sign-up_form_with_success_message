5852c2e2a1789b609810341acae7e8228b9612c6
Ramiro Alves <110850490+RamaAlves@users.noreply.github.com>
2024-03-16 00:35:57 -0300

    styles desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import styles from './App.module.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState<string>('');
+  const [validEmail, setValidEmail] = useState<boolean>(false);
+  const [errorMail, setErrorMail] = useState<boolean>(false);
+  const [showSuscribed, setShowSuscribed] = useState<boolean>(false);
+
+  const regexMail =
+    /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
+
+  function checkEmail(email: string) {
+    if (regexMail.test(email)) {
+      setValidEmail(true);
+      setErrorMail(false);
+      setEmail(email);
+    } else {
+      setValidEmail(false);
+      setErrorMail(true);
+    }
+  }
+
+  function sendForm() {
+    if (validEmail) setShowSuscribed(true);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className={styles.main}>
+      {!showSuscribed ? (
+        <div className={styles.card}>
+          <section>
+            <h1>Stay updated!</h1>
+            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+            <ul className={styles.list}>
+              <li className={styles.itemList}>
+                <img src="/assets/images/icon-list.svg" alt="icono list" />
+                <p>Product discovery and building what matters</p>
+              </li>
+              <li className={styles.itemList}>
+                <img src="/assets/images/icon-list.svg" alt="icono list" />
+                <p>Measuring to ensure updates are a success</p>
+              </li>
+              <li className={styles.itemList}>
+                <img src="/assets/images/icon-list.svg" alt="icono list" />
+                <p>And much more!</p>
+              </li>
+            </ul>
+            <form onSubmit={sendForm}>
+              <label htmlFor="email">Email adress</label>
+              <input
+                className={[
+                  errorMail && styles.errorInput,
+                  styles.inputMail,
+                ].join(' ')}
+                type="email"
+                id="email"
+                onBlur={e => {
+                  checkEmail(e.target.value);
+                }}
+                placeholder="email@company.com"
+              />
+              {errorMail && <small>Valid email required</small>}
+              <button disabled={!validEmail}>
+                Subscribe to monthly newsletter
+              </button>
+            </form>
+          </section>
+          <img
+            src="/assets/images/illustration-sign-up-desktop.svg"
+            alt="imagen ilustrativa"
+            title="imagen ilustrativa"
+          />
+        </div>
+      ) : (
+        <div className={styles.card}>
+          <h1>Suscribed</h1>
+          <p>your email: {email}</p>
+        </div>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
