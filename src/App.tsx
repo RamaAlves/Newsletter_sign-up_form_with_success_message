c8fd96f36ca738f091864f1fc049a90b09e8b07d
Ramiro Alves <110850490+RamaAlves@users.noreply.github.com>
2024-03-27 21:48:16 -0300

    fix eslint bug

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   const [showSuscribed, setShowSuscribed] = useState<boolean>(false);
 
   const regexMail =
+    // eslint-disable-next-line no-useless-escape
     /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
 
   function checkEmail(email: string) {
